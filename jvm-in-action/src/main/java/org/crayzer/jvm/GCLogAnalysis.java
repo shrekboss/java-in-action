9bd83c39cebfdd7ee59bd9753e6f92a809690977
chenyz <>
2021-06-30 22:47:21 +0800

    first commit

---
+++ jvm-in-action/src/main/java/org/crayzer/jvm/GCLogAnalysis.java
@@ -0,0 +1,67 @@
+package org.crayzer.jvm;
+
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.LongAdder;
+
+/**
+ * java -XX:+UseSerialGC -Xms512m -Xmx512m -Xloggc:gc.serial.log -XX:+PrintGCDetails -XX:+PrintGCDateStamps GCLogAnalysis
+ * java -XX:+UseParallelGC -Xms512m -Xmx512m -Xloggc:gc.parallel.log -XX:+PrintGCDetails -XX:+PrintGCDateStamps GCLogAnalysis
+ * java -XX:+UseConcMarkSweepGC -Xms512m -Xmx512m -Xloggc:gc.concMarkSweep.log -XX:+PrintGCDetails -XX:+PrintGCDateStamps GCLogAnalysis
+ * java -XX:+UseG1GC -Xms512m -Xmx512m -Xloggc:gc.g1.log -XX:+PrintGCDetails -XX:+PrintGCDateStamps GCLogAnalysis
+ */
+public class GCLogAnalysis {
+
+    private static Random random = new Random();
+
+    public static void main(String[] args) {
+        long startMillis = System.currentTimeMillis();
+        long timeoutMillis = TimeUnit.SECONDS.toMillis(1);
+        long endMillis = startMillis + timeoutMillis;
+
+        LongAdder counter = new LongAdder();
+        System.out.println("正在执行...");
+
+        int cacheSize = 2000;
+        Object[] cacheGarbage = new Object[cacheSize];
+
+        while (System.currentTimeMillis() < endMillis) {
+            Object garbage = generateGarbage(100 * 1024);
+            counter.increment();
+            int randomIndex = random.nextInt(2 * cacheSize);
+            if (randomIndex < cacheSize) {
+                cacheGarbage[randomIndex] = garbage;
+            }
+        }
+        System.out.println("执行结束！共产生对象次数：" + counter.longValue());
+    }
+
+    private static Object generateGarbage(int max) {
+        int randomSize = random.nextInt(max);
+        int type = randomSize % 4;
+        Object result;
+
+        switch (type) {
+            case  0:
+                result = new int[randomSize];
+                break;
+            case 1:
+                result = new byte[randomSize];
+                break;
+            case 2:
+                result = new double[randomSize];
+                break;
+            default:
+                StringBuilder builder = new StringBuilder();
+                String randomString = "randomString-Anything";
+                while (builder.length() < randomSize) {
+                    builder.append(randomString);
+                    builder.append(max);
+                    builder.append(randomSize);
+                }
+                result = builder.toString();
+                break;
+        }
+        return result;
+    }
+}
