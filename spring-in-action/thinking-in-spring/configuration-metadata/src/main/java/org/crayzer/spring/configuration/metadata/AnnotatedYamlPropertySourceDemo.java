f1267bf62d88d999c7626f459f1f8445a6138be4
Crayzer <cyz_shrek2012@foxmail.com>
2021-07-27 18:18:07 +0800

    feat(configuration-metadata): polish code

--- spring-in-action/thinking-in-spring/configuration-metadata/src/main/java/org/crayzer/spring/configuration/metadata/AnnotatedYamlPropertySourceDemo.java
+++ spring-in-action/thinking-in-spring/configuration-metadata/src/main/java/org/crayzer/spring/configuration/metadata/AnnotatedYamlPropertySourceDemo.java
@@ -1,7 +1,49 @@
 package org.crayzer.spring.configuration.metadata;
 
+import org.crayzer.spring.ioc.domain.City;
+import org.crayzer.spring.ioc.domain.User;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.PropertySource;
+
 /**
+ * 基于 Java 注解的 YAML 外部化配置示例
+ *
  * @author crayzer
  */
+@PropertySource(
+        name = "yamlPropertySource",
+        value = "classpath:/META-INF/user.yaml",
+        factory = YamlPropertySourceFactory.class)
 public class AnnotatedYamlPropertySourceDemo {
+
+    /**
+     * user.name 是 Java Properties 默认存在，当前用户：XXX，而非配置文件中定义"马克图布"
+     *
+     * @param id
+     * @param name
+     * @return
+     */
+    @Bean
+    public User user(@Value("${user.id}") Long id, @Value("${user.name}") String name, @Value("${user.city}") City city) {
+        User user = new User();
+        user.setId(id);
+        user.setName(name);
+        user.setCity(city);
+        return user;
+    }
+
+    public static void main(String[] args) {
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
+        // 注册当前类作为 Configuration Class
+        context.register(AnnotatedYamlPropertySourceDemo.class);
+        // 启动 Spring 应用上下文
+        context.refresh();
+        // 获取 Map YAML 对象
+        User user = context.getBean("user", User.class);
+        System.out.println(user);
+        // 关闭 Spring 应用上下文
+        context.close();
+    }
 }
