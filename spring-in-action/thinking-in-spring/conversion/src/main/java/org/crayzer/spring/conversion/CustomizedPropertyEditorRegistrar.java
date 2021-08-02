759cb3dac067b82a57ce926520908e400a7f6069
Crayzer <cyz_shrek2012@foxmail.com>
2021-08-02 22:51:53 +0800

    feat(conversion): init

---
+++ spring-in-action/thinking-in-spring/conversion/src/main/java/org/crayzer/spring/conversion/CustomizedPropertyEditorRegistrar.java
@@ -0,0 +1,22 @@
+package org.crayzer.spring.conversion;
+
+import org.crayzer.spring.ioc.domain.User;
+import org.springframework.beans.PropertyEditorRegistrar;
+import org.springframework.beans.PropertyEditorRegistry;
+
+/**
+ * 自定义 {@link PropertyEditorRegistrar} 实现
+ *
+ * @see PropertyEditorRegistrar
+ * @author crayzer
+ */
+// @Component // 3. 将其声明为 Spring Bean
+public class CustomizedPropertyEditorRegistrar implements PropertyEditorRegistrar {
+
+    @Override
+    public void registerCustomEditors(PropertyEditorRegistry registry) {
+        // 1. 通用类型转换
+        // 2. Java Bean 属性类型转换
+        registry.registerCustomEditor(User.class, "context", new StringToPropertiesPropertyEditor());
+    }
+}
