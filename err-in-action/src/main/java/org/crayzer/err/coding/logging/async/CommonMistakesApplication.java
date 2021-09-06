dfdd099d908e89b7948c2a76d1fa10b57a8485ed
Crayzer <cyz_shrek2012@foxmail.com>
2021-09-06 21:38:11 +0800

    style(err-in-action): review

--- err-in-action/src/main/java/org/crayzer/err/coding/logging/async/CommonMistakesApplication.java
+++ err-in-action/src/main/java/org/crayzer/err/coding/logging/async/CommonMistakesApplication.java
@@ -9,6 +9,7 @@ public class CommonMistakesApplication {
     public static void main(String[] args) {
         // 同步 Appender /logging/performance
         // System.setProperty("logging.config", "classpath:org/crayzer/err/coding/logging/async/performance_sync.xml");
+
         // 异步 Appender /logging/performance
         // System.setProperty("logging.config", "classpath:org/crayzer/err/coding/logging/async/performance_async.xml");
 
