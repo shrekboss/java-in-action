03c06ced9b16905749b9d2e195280033d2bfab39
Crayzer <cyz_shrek2012@foxmail.com>
2021-10-01 22:56:10 +0800

    feat(err-in-action): 异步处理需要消息补偿闭环

---
+++ err-in-action/src/main/java/org/crayzer/err/design/asynprocess/compensation/CompensationJob.java
@@ -0,0 +1,40 @@
+package org.crayzer.err.design.asynprocess.compensation;
+
+import jodd.util.concurrent.ThreadFactoryBuilder;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+
+@Component
+@Slf4j
+public class CompensationJob {
+    //补偿Job异步处理线程池
+    private static ThreadPoolExecutor compensationThreadPool = new ThreadPoolExecutor(
+            10, 10,
+            1, TimeUnit.HOURS,
+            new ArrayBlockingQueue<>(1000),
+            new ThreadFactoryBuilder().setNameFormat("compensation-threadpool-%d").get());
+    @Autowired
+    private UserService userService;
+    @Autowired
+    private MemberService memberService;
+    //目前补偿到哪个用户ID
+    private long offset = 0;
+
+    //10秒后开始补偿，5秒补偿一次
+    @Scheduled(initialDelay = 10_000, fixedRate = 5_000)
+    public void compensationJob() {
+        log.info("开始从用户ID {} 补偿", offset);
+        //获取从offset开始的用户
+        userService.getUsersAfterIdWithLimit(offset, 5).forEach(user -> {
+            compensationThreadPool.execute(() -> memberService.welcome(user));
+            offset = user.getId();
+        });
+    }
+}
---
+++ err-in-action/src/main/java/org/crayzer/err/design/asynprocess/compensation/UserController.java
@@ -0,0 +1,40 @@
+package org.crayzer.err.design.asynprocess.compensation;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.stream.IntStream;
+
+
+@RestController
+@Slf4j
+@RequestMapping("user")
+public class UserController {
+    @Autowired
+    private UserService userService;
+    @Autowired
+    private RabbitTemplate rabbitTemplate;
+
+    /**
+     * curl http://localhost:45678/user/register
+     */
+    @GetMapping("register")
+    public void register() {
+        //模拟10个用户注册
+        IntStream.rangeClosed(1, 10).forEach(i -> {
+            //落库
+            User user = userService.register();
+            //模拟50%的消息可能发送失败
+            if (ThreadLocalRandom.current().nextInt(10) % 2 == 0) {
+                //通过RabbitMQ发送消息
+                rabbitTemplate.convertAndSend(RabbitConfiguration.EXCHANGE, RabbitConfiguration.ROUTING_KEY, user);
+                log.info("sent mq user {}", user.getId());
+            }
+        });
+    }
+}
