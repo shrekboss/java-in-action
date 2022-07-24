ce741e25eec6bcdf1dba4ea0f39a38dc22c46eec
yeqi <crayzer.chen@gmail.com>
2022-07-24 11:42:07 +0800

    feat(java-in-action): Spring Event（Application Event）其实就是一个观察者设计模式，一个 Bean 处理完成任务后希望通知其它 Bean 或者说一个 Bean 想观察监听另一个Bean 的行为

--- spring-in-action/spring-event-template/src/test/java/org/crayzer/template/publisher/OrderServiceTest.java
+++ spring-in-action/spring-event-template/src/test/java/org/crayzer/template/publisher/OrderServiceTest.java
@@ -1,14 +1,17 @@
 package org.crayzer.template.publisher;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
 /**
  * @author <a href="mailto:yeqi@banniuyun.com">夜骐</a>
  * @since 1.0.0
  */
 @SpringBootTest
+@RunWith(SpringRunner.class)
 public class OrderServiceTest {
 
     @Autowired
