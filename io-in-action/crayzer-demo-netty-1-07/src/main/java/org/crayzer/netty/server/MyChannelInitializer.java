ca6059f95c359280f9c46d19558dcb7ca79ebcfc
夜骐 <crayzer.chen@gmail.com>
2022-08-04 13:07:00 +0800

    feat(io-in-action): NettyClient

---
+++ io-in-action/crayzer-demo-netty-1-07/src/main/java/org/crayzer/netty/server/MyChannelInitializer.java
@@ -0,0 +1,18 @@
+package org.crayzer.netty.server;
+
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.socket.SocketChannel;
+
+
+/**
+ * @author <a href="mailto:yeqi@banniuyun.com">夜骐</a>
+ * @since 1.0.0
+ */
+public class MyChannelInitializer extends ChannelInitializer<SocketChannel> {
+    @Override
+    protected void initChannel(SocketChannel ch) throws Exception {
+        System.out.println("链接报告开始");
+        System.out.println("链接报告信息：本客户端链接到服务端。channelId：" + ch.id());
+        System.out.println("链接报告完毕");
+    }
+}
