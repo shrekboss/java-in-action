0631f92d3a4a958fb45572f30ec72d6c252c84e8
夜骐 <crayzer.chen@gmail.com>
2022-08-04 11:09:49 +0800

    feat(io-in-action): NettyServer收发数据

---
+++ io-in-action/crayzer-demo-netty-1-04/src/main/java/org/crayzer/netty/server/NettyServer.java
@@ -0,0 +1,43 @@
+package org.crayzer.netty.server;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+
+/**
+ * NettyServer收发数据
+ *
+ * @author <a href="mailto:yeqi@banniuyun.com">夜骐</a>
+ * @since 1.0.0
+ */
+public class NettyServer {
+
+    public static void main(String[] args) {
+        new NettyServer().bing(7397);
+    }
+
+    private void bing(int port) {
+
+        EventLoopGroup parentGroup = new NioEventLoopGroup();
+        EventLoopGroup childGroup = new NioEventLoopGroup();
+
+        try {
+            ServerBootstrap b = new ServerBootstrap();
+            b.group(parentGroup, childGroup)
+                    .channel(NioServerSocketChannel.class)
+                    .option(ChannelOption.SO_BACKLOG, 128)
+                    .childHandler(new MyChannelInitializer());
+            ChannelFuture f = b.bind(port).sync();
+            System.out.println("crayzer-demo-netty-04 server start done. ");
+            f.channel().closeFuture().sync();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            childGroup.shutdownGracefully();
+            parentGroup.shutdownGracefully();
+        }
+    }
+}
